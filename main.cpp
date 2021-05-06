92d03ac0c5cb6d44eb721d4d8f33a79a547ecef7
Ula0505 <ulana.duenkova@gmail.com>
2021-05-06 17:51:31 +0300

    4

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <stdio.h>
+using namespace std;
 int main() {
+	int firstNum, secondNum;
+	cout << "Enter the first number: ";
+	cin >> firstNum;
+	cout << "Enter the second number: ";
+	cin >> secondNum;
 	return 0;
 }
