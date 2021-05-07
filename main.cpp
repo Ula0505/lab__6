b35f3e2a3f90313fa8345c737035dad7ec08874f
Ula0505 <ulana.duenkova@gmail.com>
2021-05-07 13:52:29 +0300

    7

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 using namespace std;
 int main() {
-	int firstNum, secondNum, Sum, Min;
+	int firstNum, secondNum, Sum, Min, Comp, Priv;
 	cout << "Enter the first number: ";
 	cin >> firstNum;
 	cout << "Enter the second number: ";
@@ -10,5 +10,9 @@ int main() {
 	cout << "The sum of the two numbers entered is: " << Sum;
 	Min = firstNum - secondNum;
 	cout << "The difference between the two entered numbers is: " << Min;
+	Comp = firstNum * secondNum;
+	cout << "The product of the two entered numbers is equal to: " << Comp;
+	Priv = firstNum / secondNum;
+	cout << "The quotient of the two entered numbers is: " << Priv; 
 	return 0;
 }
